d3242352501f1294719fb9cc9f9dcbf20bfd1547
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-04-01 15:24:53 -0400

    retrieving data feature

--- scrapbookApp/ViewController.swift
+++ scrapbookApp/ViewController.swift
@@ -7,23 +7,73 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var titleArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // add plus sign to navigation bar, top right area of screen -- use action addButtonCLicked
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
+        getData()
     }
+    
+    // retrieve data from CoreData database
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate // access AppDelegat.swift
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Photos")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject]{
+                if let title = result.value(forKey: "title") as? String {
+                    self.titleArray.append(title)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                
+                self.tableView.reloadData() // reload data of tableview to account for addition of new data
+                
+            }
+            
+        } catch {
+            print("error")
+        }
+        
+    }
+    
     
     // action to bring user to detailsVC when plus sign on navigation bar is clicked
     @objc func addButtonClicked() {
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+        
     }
 
 
