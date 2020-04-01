ec50ce244f22f39b3e33a1c22d58cd8b5e5cede6
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-04-01 14:23:14 -0400

    image upload feature
    added image gesture recognizer for picker action, allow access to user's image library

--- scrapbookApp/DetailsVC.swift
+++ scrapbookApp/DetailsVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleText: UITextField!
@@ -18,11 +18,34 @@ class DetailsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Recognizers
         
         // hide keyboard when tap gesture is recognized anywhere in the view
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        // imageView gesture recognizer to upload an image
+        imageView.isUserInteractionEnabled = true // allow imageView to be interacted with
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage)) // refer to selectImage action
+        imageView.addGestureRecognizer(imageTapRecognizer) // add tap gesture recognizer to imageView object
+        
+    }
+    
+    @objc func selectImage() {
+        // UIImagePickerContoller gives access to managing images and videos -- use to access user's media library
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true // if user selects an image, they have the ability to edit it (ex. zoom, crop, etc.)
+        present(picker, animated: true, completion: nil) // present the picker in an animated style, do not do anything upon completion
+        
+    }
+    
+    // tells the delegate that the user picked an image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage // cast it optionally as a UIImage in case user uploads invalid file or cancels upload
+        self.dismiss(animated: true, completion: nil) // dismiss picker when finished choosing image
         
     }
     
